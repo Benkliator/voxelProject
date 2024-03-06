23351e579ea0bf4c355e08a8e201728d1162599c
Benkliator <Benjaminekerna@gmail.com>
2024-03-06 23:39:58 +0100

    github repo now fully setup

--- lib/skybox.h
+++ lib/skybox.h
@@ -14,13 +14,15 @@ class Skybox{
     public:
         Skybox(std::vector<const char*> texturePath);
 
+         ~Skybox();
+
         // Does what you think it does.
         void draw(glm::mat4 view);
     private:
-        unsigned int vertexArrayObjID;
-        unsigned int vertexBufferObjID;
-        unsigned int elementBufferObjID;
+        unsigned int VAO;
+        unsigned int VBO;
+        unsigned int EBO;
 
-        unsigned int shaderProgram;        
+        unsigned int shaderProgram;
         unsigned int texture;
 };
--- src/skybox.cc
+++ src/skybox.cc
@@ -30,7 +30,7 @@ Skybox::Skybox(std::vector<const char*> texturePath)
         0, 5, 4,
     };
     unsigned int vertexShader = loadShader(GL_VERTEX_SHADER, "./res/shaders/skybox.vert");
-    unsigned int fragmentShader = loadShader(GL_FRAGMENT_SHADER, "./res/shaders/skybox.frag");    
+    unsigned int fragmentShader = loadShader(GL_FRAGMENT_SHADER, "./res/shaders/skybox.frag");
     shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
@@ -38,17 +38,17 @@ Skybox::Skybox(std::vector<const char*> texturePath)
     texture = loadTextureCube(texturePath, GL_RGB);
 
     // Generate buffers
-    glGenVertexArrays(1, &vertexArrayObjID);
-    glGenBuffers(1, &vertexBufferObjID);
-    glGenBuffers(1, &elementBufferObjID);
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     // Init drawing
-    glBindVertexArray(vertexArrayObjID);
+    glBindVertexArray(VAO);
 
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjID);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObjID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Position
@@ -56,12 +56,20 @@ Skybox::Skybox(std::vector<const char*> texturePath)
     glEnableVertexAttribArray(0);
 }
 
+Skybox::~Skybox()
+{
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteShader(shaderProgram);
+}
+
 void Skybox::draw(glm::mat4 view)
 {
     glUseProgram(shaderProgram);
     glBindTexture(GL_TEXTURE_CUBE_MAP, texture);
-    glBindVertexArray(vertexArrayObjID);
-     
+    glBindVertexArray(VAO);
+
     glDepthMask(false);
     glCullFace(GL_FRONT);
     glm::mat4 scaling = glm::mat4(1.0f, 0.0f, 0.0f, 0.0f,
@@ -69,14 +77,14 @@ void Skybox::draw(glm::mat4 view)
                                 0.0f, 0.0f, 1.0f, 0.0f,
                                 0.0f, 0.0f, 0.0f, 0.001f);
     glm::mat4 projection = glm::perspective(glm::radians(45.0f), 1.8f, 0.1f, 1000.0f);
-    
+
     glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, &projection[0][0]);
     glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, &view[0][0]);
     glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "scaling"), 1, GL_FALSE, glm::value_ptr(scaling));
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObjID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-    
+
     glCullFace(GL_BACK);
     glDepthMask(true);
 }
