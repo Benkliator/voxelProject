0916a426ee1b1076ec347e8d0b6420277930d904
Benkliator <Benjaminekerna@gmail.com>
2024-03-06 23:33:31 +0100

    previous issue resolved

--- lib/skybox.h
+++ lib/skybox.h
@@ -14,15 +14,13 @@ class Skybox{
     public:
         Skybox(std::vector<const char*> texturePath);
 
-        ~Skybox();
-
         // Does what you think it does.
         void draw(glm::mat4 view);
     private:
-        unsigned int VAO;
-        unsigned int VBO;
-        unsigned int EBO;
+        unsigned int vertexArrayObjID;
+        unsigned int vertexBufferObjID;
+        unsigned int elementBufferObjID;
 
-        unsigned int shaderProgram;
+        unsigned int shaderProgram;        
         unsigned int texture;
 };
