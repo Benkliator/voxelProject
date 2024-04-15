1cfd20e5b8b5a45503e4b1170230959da5c4c43a
Benjamin <benkliator@CatPC.lan>
2024-04-15 16:09:20 +0200

    reverted to using mipmaps for textures due to it looking horrible without it (only on linux for some reason)

--- src/utility.cc
+++ src/utility.cc
@@ -47,17 +47,20 @@ unsigned int loadShader(GLenum type, const char* filePath) {
 unsigned int loadTexture(const char* filepath, unsigned int format) {
     unsigned int texture;
     int width, height, nrChannels;
-    unsigned char* data = stbi_load(filepath, &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load(filepath, &width, &height, &nrChannels, 0);
 
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
     glTexParameteri(
-        GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     // load and generate the texture
     if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         glTexImage2D(GL_TEXTURE_2D,
                      0,
                      format,
@@ -67,7 +70,9 @@ unsigned int loadTexture(const char* filepath, unsigned int format) {
                      format,
                      GL_UNSIGNED_BYTE,
                      data);
-    } else {
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
         std::cout << "bad texture" << std::endl;
     }
     stbi_image_free(data);
